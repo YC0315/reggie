cc44bfb846e4f960ee9debbb7eca58bdb2e87efd
yangchao <yc_lil@163.com>
2022-06-10 11:18:23 +0800

    创建医疗项目

---
+++ src/main/java/com/itheima/reggie/common/Test.java
@@ -0,0 +1,20 @@
+package com.itheima.reggie.common;
+
+public class Test {
+    public static int a = 100;
+    public static int b = 100;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < a; i++) {
+            for(int j=0; j< b; j++){
+                System.out.println(i+j);
+                for(int x=0;x < i;x++){
+                    for(int y=0;y<j;y++){
+                       int[] a = {1,2,3};
+                    }
+                }
+            }
+        }
+    }
+
+}
