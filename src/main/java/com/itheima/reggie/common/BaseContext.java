3929dedb50f0edd8e4b638e35040a45e8efa5994
yangchao <yc_lil@163.com>
2022-06-10 11:22:03 +0800

    创建外卖项目

--- src/main/java/com/itheima/reggie/common/BaseContext.java
+++ src/main/java/com/itheima/reggie/common/BaseContext.java
@@ -5,7 +5,6 @@ public class BaseContext {
     private static ThreadLocal<Long> threadLocal = new ThreadLocal<>();  // 因为id的类型时Long，因此泛型中的数据类型是Long
 
     //工具类中的方法基本上都是静态的，直接用类名调用，不用实例化对象，方便
-
     // 保存值
     public static void setCurrentId(Long id){ threadLocal.set(id);
     }
